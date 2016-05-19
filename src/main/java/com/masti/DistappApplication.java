4899bbb09c888d43744728e5afb461bee416d5d0
Maki <Maki@Maki-HP>
2016-05-19 21:58:12 +0200

    new commit

---
+++ src/main/java/com/masti/DistappApplication.java
@@ -0,0 +1,12 @@
+package com.masti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistappApplication.class, args);
+	}
+}
